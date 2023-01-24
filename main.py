0b23dab4216b56223d9b03c20d3965f92808afc2
Nico Schulz <123396129+saltyniggo@users.noreply.github.com>
2023-01-24 09:05:11 +0100

    create main.py with basic code
    finished version with just the basics.

---
+++ main.py
@@ -0,0 +1,29 @@
+#Mad Libs Generator
+#this is only a little project, so that ive done it
+
+#title
+print('Lets make a Mad Libs Story!')
+print('')
+print('')
+
+#get the user input
+name = input('Please enter a Name: ')
+adjective1 = input('Please enter an adjective: ')
+noun1 = input('Please enter a noun: ')
+plural_noun1 = input('Please enter a plural noun: ')
+adjective2 = input('Please enter an adjective: ')
+adjective3 = input('Please enter an adjective: ')
+adjective4 = input('Please enter an adjective: ')
+noun2 = input('Please enter a noun: ')
+noun3 = input('Please enter a noun: ')
+adjective5 = input('Please enter an adjective: ')
+
+#Print the story
+print('')
+print('Once upon a time there was a brave knight named', name, 'who lived in a', adjective1, 'castle.')
+print('Every day, he would wake up and go on', noun1, 'adventures to slay', plural_noun1, '.')
+print('He was always', adjective2, 'and', adjective3, 'and his mission was to make the world a better place.')
+print('')
+print('One day, he encountered a', adjective4, 'dragon.')
+print('He used his', noun2, 'and', noun3, 'to defeat the dragon, and everyone in the kingdom was happy and', adjective5, '.')
+print('The brave knight', name, 'had saved the day!')
